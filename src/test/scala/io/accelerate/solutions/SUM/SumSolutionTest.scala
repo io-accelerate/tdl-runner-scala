5a60f06777016dd3361ac99956326bbbb07f8a6b
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2025-04-02 17:18:54 +0100

    Releasing version v0.29.1.1

--- src/test/scala/io/accelerate/solutions/SUM/SumSolutionTest.scala
+++ src/test/scala/io/accelerate/solutions/SUM/SumSolutionTest.scala
@@ -6,6 +6,6 @@ import org.scalatest.matchers.should.Matchers
 class SumSolutionTest extends AnyFlatSpec with Matchers {
 
   "Sum" should "compute sum" in {
-    SumSolution.sum(1, 2) shouldBe 3
+    SumSolution().sum(1, 2) shouldBe 3
   }
 }
